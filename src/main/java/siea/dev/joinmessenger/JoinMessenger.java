c9c39d4eff3f75b00b8a9b636ebe49c98381592c
SIEA <69807609+DrVosss@users.noreply.github.com>
2024-03-08 21:53:11 +0100

    initial 2

--- src/main/java/siea/dev/joinmessenger/JoinMessenger.java
+++ src/main/java/siea/dev/joinmessenger/JoinMessenger.java
@@ -1,17 +1,26 @@
 package siea.dev.joinmessenger;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import siea.dev.joinmessenger.listeners.PlayerConnectionEvents;
 
 public final class JoinMessenger extends JavaPlugin {
 
+    private static Plugin plugin;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        plugin = this;
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(new PlayerConnectionEvents(), this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static Plugin getPlugin(){
+        return plugin;
     }
 }
--- src/main/java/siea/dev/joinmessenger/listeners/PlayerConnectionEvents.java
+++ src/main/java/siea/dev/joinmessenger/listeners/PlayerConnectionEvents.java
@@ -1,2 +1,60 @@
-package siea.dev.joinmessenger.listeners;public class PlayerConnectionEvents {
+package siea.dev.joinmessenger.listeners;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.Plugin;
+import siea.dev.joinmessenger.JoinMessenger;
+
+public class PlayerConnectionEvents implements Listener {
+    private final String joinMessageTemplate;
+    private final String firstJoinMessageTemplate;
+    private final String quitMessageTemplate;
+
+    public PlayerConnectionEvents(){
+        Plugin plugin = JoinMessenger.getPlugin();
+        FileConfiguration config = plugin.getConfig();
+
+        joinMessageTemplate = config.getString("joinMessage").replace("&","§");
+        firstJoinMessageTemplate = config.getString("firstJoinMessage").replace("&","§");
+        quitMessageTemplate = config.getString("quitMessage").replace("&","§");
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerJoin(PlayerJoinEvent e){
+        Player player = e.getPlayer();
+        if (player.hasPermission("joinMessenger.hideJoin")){
+            e.setJoinMessage(null);
+        }
+        else if (player.hasPlayedBefore()){
+            e.setJoinMessage(replacePlaceholders(joinMessageTemplate, player));
+        }
+        else{
+            e.setJoinMessage(replacePlaceholders(firstJoinMessageTemplate, player));
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerQuit(PlayerQuitEvent e){
+        Player player = e.getPlayer();
+        if (player.hasPermission("joinMessenger.hideJoin")){
+            e.setQuitMessage(null);
+        }
+        else{
+            e.setQuitMessage(replacePlaceholders(quitMessageTemplate, player));
+        }
+
+    }
+
+    private String replacePlaceholders(String message, Player player){
+        if (message == null || player == null) return null;
+        message = message.replace("%player%", player.getDisplayName());
+        message = message.replace("%playercount%", String.valueOf(player.getServer().getOnlinePlayers().size()));
+        message = message.replace("%maxplayers%", String.valueOf(player.getServer().getMaxPlayers()));
+        return message;
+    }
 }
